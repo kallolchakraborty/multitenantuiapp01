77d81a1b709721e8a9dd5d281b904b6e1fd21260
Kallol Chakraborty <kallolathome@live.com>
2022-07-14 07:59:55 +0000

    Initial Commit

---
+++ srv/library.js
@@ -0,0 +1,123 @@
+module.exports = {
+
+    createRoute: createRoute,
+    deleteRoute: deleteRoute };
+
+const cfenv = require('cfenv');
+const appEnv = cfenv.getAppEnv();
+
+const core = require('@sap-cloud-sdk/core');
+
+
+
+
+async function getCFInfo(appname) {
+    try {
+        // get app GUID
+        let res1 = await core.executeHttpRequest({ destinationName: 'multitenantuiapp01-cfapi'}, {
+            method: 'GET',
+            url: '/v3/apps?organization_guids=' + appEnv.app.organization_id + '&space_guids=' + appEnv.app.space_id + '&names=' + appname
+        });
+        // get domain GUID
+        let res2 = await core.executeHttpRequest({ destinationName: 'multitenantuiapp01-cfapi'}, {
+            method: 'GET',
+            url: '/v3/domains?names=' + /\.(.*)/gm.exec(appEnv.app.application_uris[0])[1]
+        });
+        let results = {
+            'app_id': res1.data.resources[0].guid,
+            'domain_id': res2.data.resources[0].guid
+        };
+        return results;
+    } catch (err) {
+        console.log(err.stack);
+        return err.message;
+    }
+};
+
+async function createRoute(tenantHost, appname) {
+    getCFInfo(appname).then(
+        async function (CFInfo) {
+            try {
+                // create route
+                let res1 = await core.executeHttpRequest({ destinationName: 'multitenantuiapp01-cfapi'}, {
+                    method: 'POST',
+                    url: '/v3/routes',
+                    data: {
+                        'host': tenantHost,
+                        'relationships': {
+                            'space': {
+                                'data': {
+                                    'guid': appEnv.app.space_id
+                                }
+                            },
+                            'domain': {
+                                'data': {
+                                    'guid': CFInfo.domain_id
+                                }
+                            }
+                        }
+                    },
+                });
+                // map route to app
+                let res2 = await core.executeHttpRequest({ destinationName: 'multitenantuiapp01-cfapi'}, {
+                    method: 'POST',
+                    url: '/v3/routes/' + res1.data.guid + '/destinations',
+                    data: {
+                        'destinations': [{
+                            'app': {
+                                'guid': CFInfo.app_id
+                            }
+                        }]
+                    },
+                });
+                console.log('Route created for ' + tenantHost);
+                return res2.data;
+            } catch (err) {
+                console.log(err.stack);
+                return err.message;
+            }
+        },
+        function (err) {
+            console.log(err.stack);
+            return err.message;
+        });
+};
+
+async function deleteRoute(tenantHost, appname) {
+    getCFInfo(appname).then(
+        async function (CFInfo) {
+            try {
+                // get route id
+                let res1 = await core.executeHttpRequest({ destinationName: 'multitenantuiapp01-cfapi'}, {
+                    method: 'GET',
+                    url: '/v3/apps/' + CFInfo.app_id + '/routes?hosts=' + tenantHost
+                });
+                if (res1.data.pagination.total_results === 1) {
+                    try {
+                        // delete route
+                        let res2 = await core.executeHttpRequest({ destinationName: 'multitenantuiapp01-cfapi'}, {
+                            method: 'DELETE',
+                            url: '/v3/routes/' + res1.data.resources[0].guid
+                        });
+                        console.log('Route deleted for ' + tenantHost);
+                        return res2.data;
+                    } catch (err) {
+                        console.log(err.stack);
+                        return err.message;
+                    }
+                } else {
+                    let errmsg = { 'error': 'Route not found' };
+                    console.log(errmsg);
+                    return errmsg;
+                }
+            } catch (err) {
+                console.log(err.stack);
+                return err.message;
+            }
+        },
+        function (err) {
+            console.log(err.stack);
+            return err.message;
+        });
+};
+
